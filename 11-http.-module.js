f4338e35f04a28f27182ba529e35d51241378b27
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-09-01 15:36:42 +0530

    added npm, async await

--- 11-http.-module.js
+++ 11-http.-module.js
@@ -1,18 +1,42 @@
-const http = require('http')
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/') {
+//         res.end('Hyy, I am listening to you!!')
+//     }
+//     if (req.url === '/about') {
+//         res.end('This is the node js fundamental')
+//     }
+    
+//     res.end(`
+//     <h1>Oops!</h1>
+//     <p>we can't see the page you are looking for...</p>
+//     <a href="/">Back to Home</a>
+//     `)
+// })
+
+// server.listen(5000)
+
+
+const http = require("http")
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.end('Hyy, I am listening to you!!')
+    if (req.url === "/") {
+        console.log("Home Page")
+        res.write("Home page")
+        res.end() // End the response here for the "/" route
+    } else if (req.url === '/about') {
+        console.log("About page")
+        res.write("About page")
+        res.end() // End the response here for the "/about" route
+    } else {
+        console.log("Error Page")
+        res.write("Error Page")
+        res.end() // End the response for any other route
     }
-    if (req.url === '/about') {
-        res.end('This is the node js fundamental')
-    }
-    
-    res.end(`
-    <h1>Oops!</h1>
-    <p>we can't see the page you are looking for...</p>
-    <a href="/">Back to Home</a>
-    `)
-})
+}
+)
 
-server.listen(5000)
+server.listen(5000, () => {
+    console.log("Server is listening to this port 5000:")
+})
--- app.js
+++ app.js
@@ -7,10 +7,23 @@
 //global dependency - use it in any project
 //npm install -g <packageName>
 
-const _ = require("lodash")
+// const _ = require("lodash")
 
-const items = [1, [2, 3, [4]]]
-const newItems = _.flattenDeep(items)
+// const items = [1, [2, 3, [4]]]
+// const newItems = _.flattenDeep(items)
 
-console.log(newItems)
-console.log("Hyy")
+// console.log(newItems)
+// console.log("Hyy")
+
+const { readFile, writeFile } = require('fs').promises
+//const writeFile = require('fs').promises
+
+const getText = async () => {
+    const first = await readFile('./content/first.txt', 'utf8')
+    const second = await readFile('./content/second.txt', 'utf8')
+    await writeFile('./content/mind-granade.txt', `Here is the changes: ${first}, ${second}`)
+}
+
+getText()
+
+//Note-  we can use util module also like util.promisify and other approach is create new Promise()
